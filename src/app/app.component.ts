52050e93e870b40e2ec38c8fbef37aa50bc4154a
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-07-25 23:47:14 +0300

    chore: mapState variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { YandexMapModule } from './ng-yandex-map-module/services/yandex-map/yandex-map.type';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public mapOptions: any = {
+  public mapState: YandexMapModule.IYandexMapState = {
     center: [60.169931, 24.938513],
     zoom: 13
+  };
+
+  public mapOptions: YandexMapModule.IYandexMapOptions = {
+    maxZoom: 15
   };
 }
