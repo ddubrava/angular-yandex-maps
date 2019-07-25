3616c31c97029e161b9746c62d67e8a95b4b0baf
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-07-25 21:47:51 +0300

    chore: generate NgYandexMapModule

---
+++ src/app/ng-yandex-map-module/ng-yandex-map.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgYandexMapComponent } from './ng-yandex-map/ng-yandex-map.component';
+
+@NgModule({
+  declarations: [
+    NgYandexMapComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NgYandexMapModule { }
