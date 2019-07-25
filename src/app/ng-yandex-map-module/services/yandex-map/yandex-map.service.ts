2a3b60c8f4e9b554f4d192abef6ea8dd721256f2
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-07-25 22:45:21 +0300

    feat: implement yandexMapService

--- src/app/ng-yandex-map-module/services/yandex-map/yandex-map.service.ts
+++ src/app/ng-yandex-map-module/services/yandex-map/yandex-map.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+declare const ymaps: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class YandexMapService {
+  private _map$: Subject<any> = new Subject();
+  private _scriptYmaps: HTMLScriptElement;
 
-  constructor() { }
+  constructor() {}
+
+  private _loadScript(): void {
+    this._scriptYmaps = document.createElement('script');
+    this._scriptYmaps.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU';
+    document.body.appendChild(this._scriptYmaps);
+  }
+
+  private _createMap(element: string, options: any): void {
+    const map = new ymaps.Map(
+      element,
+      options
+    );
+
+    this._map$.next(map);
+  }
+
+  public initMap(element: string, options: any): Subject<any> {
+    this._loadScript();
+
+    this._scriptYmaps.onload = () => {
+      ymaps.ready(() => {
+        this._createMap(element, options);
+      });
+    };
+
+    return this._map$;
+  }
 }
