7248bddd7130967e8caf638451a82872cbf89f64
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-07-28 00:46:22 +0300

    chore: rename project in angular.json & test files

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-yandex-map!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-yandex-maps!');
   });
 
   afterEach(async () => {
