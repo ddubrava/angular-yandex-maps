d9c6929959458d1d98d8b060776387bc6dac0e95
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-07-28 01:08:28 +0300

    chore: replace module by module from lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AngularYandexMapsModule } from './angular-yandex-maps-module/angular-yandex-maps.module';
+import { AngularYandexMapsModule } from 'projects/angular8-yandex-maps/src/public-api';
 
 @NgModule({
   declarations: [
