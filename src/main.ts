50eb5c7780430c7b774b8fb6a1a658ec436605a7
Michael Daineka <michaeldaineka@gmail.com>
2019-08-05 14:32:03 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  console.log(13)
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
