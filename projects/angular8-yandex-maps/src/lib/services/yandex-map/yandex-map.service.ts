53c2fc8385a389bf45d68ae73d2fe0b81f98bdb7
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-08-29 00:58:05 +0300

    chore(yandex-map.service): rename element argument -> mapId

--- projects/angular8-yandex-maps/src/lib/services/yandex-map/yandex-map.service.ts
+++ projects/angular8-yandex-maps/src/lib/services/yandex-map/yandex-map.service.ts
@@ -16,28 +16,8 @@ export class YandexMapService implements YandexMapModule.IYandexMapService {
     this._apiKey = this._injector.get('API_KEY');
   }
 
-  private _loadScript(): void {
-    this._scriptYmaps = document.createElement('script');
-    this._scriptYmaps.src = `https://api-maps.yandex.ru/2.1/?apikey=${this._apiKey}&lang=ru_RU`;
-    document.body.appendChild(this._scriptYmaps);
-  }
-
-  private _createMap(
-    element: string,
-    state: YandexMapModule.IYandexMapState,
-    options: YandexMapModule.IYandexMapOptions
-  ): void {
-    const map = new ymaps.Map(
-      element,
-      state,
-      options
-    );
-
-    this._map$.next(map);
-  }
-
   public initMap(
-    element: string,
+    mapId: string,
     state: YandexMapModule.IYandexMapState,
     options?: YandexMapModule.IYandexMapOptions
   ): Subject<YandexMapModule.IYandexMap> {
@@ -45,10 +25,30 @@ export class YandexMapService implements YandexMapModule.IYandexMapService {
 
     this._scriptYmaps.onload = () => {
       ymaps.ready(() => {
-        this._createMap(element, state, options);
+        this._createMap(mapId, state, options);
       });
     };
 
     return this._map$;
+  }
+
+  private _loadScript(): void {
+    this._scriptYmaps = document.createElement('script');
+    this._scriptYmaps.src = `https://api-maps.yandex.ru/2.1/?apikey=${this._apiKey}&lang=ru_RU`;
+    document.body.appendChild(this._scriptYmaps);
+  }
+
+  private _createMap(
+    mapId: string,
+    state: YandexMapModule.IYandexMapState,
+    options: YandexMapModule.IYandexMapOptions
+  ): void {
+    const map = new ymaps.Map(
+      mapId,
+      state,
+      options
+    );
+
+    this._map$.next(map);
   }
 }
