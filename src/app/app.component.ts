8c1e380ef2addd75508923bb7f43bfdfb73c01dd
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-09-02 21:50:53 +0300

    chore(app): cleanup component, remove options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { YandexMapModule } from 'projects/angular8-yandex-maps/src/public-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public mapOptions: YandexMapModule.IYandexMapOptions = {
-    maxZoom: 12
-  };
-}
+export class AppComponent {}
