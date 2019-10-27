58e9073aaaab169bbbfee5263509e65fec63ddcb
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-10-27 17:01:42 +0300

    chore(app): move feature object in html from ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public geoObjectFeature = {
-    geometry: {
-      type: 'Rectangle',
-      coordinates: [
-          [60.183155, 24.911892],
-          [60.156454, 24.962433]
-      ]
-    }
-  };
-}
+export class AppComponent {}
