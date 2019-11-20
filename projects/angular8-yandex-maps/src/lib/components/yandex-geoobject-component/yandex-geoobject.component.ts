25b2feb9d1120464c2d2799e8c1b9bbb6a7f430c
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-11-20 14:32:41 +0300

    feat(geoobject): add geoobject events

--- projects/angular8-yandex-maps/src/lib/components/yandex-geoobject-component/yandex-geoobject.component.ts
+++ projects/angular8-yandex-maps/src/lib/components/yandex-geoobject-component/yandex-geoobject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IEvent } from '../../types/types';
 
 @Component({
   selector: 'angular-yandex-geoobject',
@@ -10,6 +11,12 @@ export class YandexGeoObjectComponent implements OnInit {
   @Input() public options: any;
 
   @Output() public load = new EventEmitter<any>();
+  @Output() public baloon = new EventEmitter<IEvent>();
+  @Output() public yaclick = new EventEmitter<IEvent>();
+  @Output() public drag = new EventEmitter<IEvent>();
+  @Output() public hint = new EventEmitter<IEvent>();
+  @Output() public mouse = new EventEmitter<IEvent>();
+  @Output() public multitouch = new EventEmitter<IEvent>();
 
   constructor() {}
 
@@ -28,6 +35,56 @@ export class YandexGeoObjectComponent implements OnInit {
     const geoObject = new ymaps.GeoObject(this.feature, this.options);
 
     map.geoObjects.add(geoObject);
+    this.emitEvents(geoObject);
+  }
+
+  /**
+   * Emit events
+   * @param multiroute - multiroute instance
+   */
+  public emitEvents(geoObject: any): void {
     this.load.emit(geoObject);
+
+    // Baloon
+    geoObject.events
+      .add(
+        ['balloonopen', 'balloonclose'],
+        (e: any) => this.baloon.emit({ instance: geoObject, type: e.originalEvent.type, event: e })
+      );
+
+    // Click
+    geoObject.events
+      .add(
+        ['click', 'dblclick'],
+        (e: any) => this.yaclick.emit({ instance: geoObject, type: e.originalEvent.type, event: e })
+      );
+
+    // Drag
+    geoObject.events
+      .add(
+        ['dragstart', 'dragend'],
+        (e: any) => this.drag.emit({ instance: geoObject, type: e.originalEvent.type, event: e })
+      );
+
+    // Hint
+    geoObject.events
+      .add(
+        ['hintopen', 'hintclose'],
+        (e: any) => this.hint.emit({ instance: geoObject, type: e.originalEvent.type, event: e })
+      );
+
+    // Mouse
+    geoObject.events
+      .add(
+        ['mousedown', 'mouseenter', 'mouseleave', 'mousemove', 'mouseup'],
+        (e: any) => this.mouse.emit({ instance: geoObject, type: e.originalEvent.type, event: e })
+      );
+
+    // Multitouch
+    geoObject.events
+      .add(
+        ['multitouchstart', 'multitouchmove', 'multitouchend'],
+        (e: any) => this.multitouch.emit({ instance: geoObject, type: e.originalEvent.type, event: e })
+      );
   }
 }
