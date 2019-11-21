f3b772fd77816b2342e21108b29bad844c7c1ea6
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-11-21 23:47:09 +0300

    chore(control): add exception for RoutePanel

--- projects/angular8-yandex-maps/src/lib/components/yandex-control-component/yandex-control.component.ts
+++ projects/angular8-yandex-maps/src/lib/components/yandex-control-component/yandex-control.component.ts
@@ -17,6 +17,11 @@ export class YandexControlComponent implements OnInit {
   public initControl(ymaps: any, map: any): void {
     const control = new ymaps.control[this.type](this.parameters);
 
+    // RoutePanel ignores state in parameters. Probably API bug
+    if (this.type === 'RoutePanel' && this.parameters && this.parameters.state) {
+      control.routePanel.state.set({ ...this.parameters.state });
+    }
+
     map.controls.add(control);
     this.load.emit(control);
   }
