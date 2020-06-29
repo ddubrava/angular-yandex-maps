f5edf9e9bba09c1758aff0f81f0c185c0f32582f
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2020-06-29 19:15:05 +0300

    fix(models): fix linting errors

--- projects/angular8-yandex-maps/src/lib/models/models.ts
+++ projects/angular8-yandex-maps/src/lib/models/models.ts
@@ -1,9 +1,9 @@
+import { InjectionToken } from '@angular/core';
+
 /**
  * Documentation for each property.
  * https://tech.yandex.ru/maps/jsapi/doc/2.1/dg/concepts/load-docpage/
  */
-import { InjectionToken } from "@angular/core";
-
 export interface IConfig {
   apikey: string;
   lang: 'ru_RU' | 'en_US' | 'en_RU' | 'ru_UA' | 'uk_UA' | 'tr_TR';
