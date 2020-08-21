3bc9b1f59ce812491acd02d3c1056c0a957da0a9
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2020-08-21 22:17:46 +0300

    feat: update to angular 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
