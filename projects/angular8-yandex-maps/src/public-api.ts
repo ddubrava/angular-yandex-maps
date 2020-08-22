833ac66ce93229f53aaa5adb74339ac7fb40d176
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2020-08-22 17:29:27 +0300

    feat: update to angular-10

---
+++ projects/angular8-yandex-maps/src/public-api.ts
@@ -0,0 +1,18 @@
+/*
+ * Public API Surface of angular8-yandex-maps
+ */
+
+// Module
+export * from './lib/angular-yandex-maps.module';
+
+// Models
+export * from './lib/models/models';
+
+// Components
+export * from './lib/components/ya-clusterer/ya-clusterer.component';
+export * from './lib/components/ya-control/ya-control.component';
+export * from './lib/components/ya-geoobject/ya-geoobject.component';
+export * from './lib/components/ya-map/ya-map.component';
+export * from './lib/components/ya-multiroute/ya-multiroute.component';
+export * from './lib/components/ya-panorama/ya-panorama.component';
+export * from './lib/components/ya-placemark/ya-placemark.component';
--- projects/angular8-yandex-maps/src/public_api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of angular8-yandex-maps
- */
-
-export * from './lib/angular-yandex-maps.module';
-export * from './lib/models/models';
