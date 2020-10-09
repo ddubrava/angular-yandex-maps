eabc459e2345307bad2393a2f14349ec87d0d8ed
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2020-10-09 19:33:40 +0300

    feat(models): improve docs for interfaces, add block tags

--- projects/angular8-yandex-maps/src/lib/models/models.ts
+++ projects/angular8-yandex-maps/src/lib/models/models.ts
@@ -1,19 +1,34 @@
 import { InjectionToken } from '@angular/core';
 
 /**
- * Documentation for each property.
- * https://tech.yandex.ru/maps/jsapi/doc/2.1/dg/concepts/load-docpage/
+ * API loading parameters
+ * @see {@link https://yandex.ru/dev/maps/jsapi/doc/2.1/dg/concepts/load.html/#load__param} for further information
  */
 export interface IConfig {
+  /**
+   * API key. You can get a key in the developer's dashboard
+   */
   apikey: string;
+  /**
+   * Locales
+   */
+  lang: 'ru_RU' | 'en_US' | 'en_RU' | 'ru_UA' | 'uk_UA' | 'tr_TR';
+  /**
+   * The order for setting geographical coordinates in API functions that accept longitude-latitude input
+   */
   coordorder?: 'latlong' | 'longlat';
+  /**
+   * List of modules to load
+   */
+  load?: string;
+  /**
+   * API loading mode
+   */
+  mode?: 'release' | 'debug';
   /**
    * Use commercial version of the API
    */
   enterprise?: boolean;
-  lang: 'ru_RU' | 'en_US' | 'en_RU' | 'ru_UA' | 'uk_UA' | 'tr_TR';
-  load?: string;
-  mode?: 'release' | 'debug';
   /**
    * Version number of the API
    */
