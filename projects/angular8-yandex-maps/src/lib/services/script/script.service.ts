5c1f6648d9d2138c621ee1a47ac910c9b15d25b3
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2020-10-23 22:07:09 +0300

    feat(script): handle error event

--- projects/angular8-yandex-maps/src/lib/services/script/script.service.ts
+++ projects/angular8-yandex-maps/src/lib/services/script/script.service.ts
@@ -1,5 +1,11 @@
 import { DOCUMENT } from '@angular/common';
-import { from, fromEvent, Observable } from 'rxjs';
+import {
+  from,
+  fromEvent,
+  merge,
+  Observable,
+  throwError
+  } from 'rxjs';
 import { IConfig, YA_MAP_CONFIG } from '../../models/models';
 import { Inject, Injectable, Optional } from '@angular/core';
 import { map, switchMap } from 'rxjs/operators';
@@ -34,7 +40,7 @@ export class ScriptService {
   /**
    * Inits Yandex.Maps script
    */
-  public initScript(): Observable<typeof ymaps> {
+  public initScript(): Observable<Event | typeof ymaps> {
     if ('ymaps' in this._window) {
       return from(ymaps.ready()).pipe(map(() => ymaps));
     }
@@ -43,13 +49,20 @@ export class ScriptService {
       const script = this._document.createElement('script');
       script.type = 'text/javascript';
       script.src = this._getScriptSource(this._config);
+      script.id = 'yandexMapsApiScript';
 
       this._script = this._document.body.appendChild(script);
     }
 
-    return fromEvent(this._script, 'load').pipe(
+    const load = fromEvent(this._script, 'load').pipe(
       switchMap(() => from(ymaps.ready()).pipe(map(() => ymaps)))
     );
+
+    const error = fromEvent(this._script, 'error').pipe(
+      switchMap(e => throwError(e))
+    );
+
+    return merge(load, error);
   }
 
   /**
