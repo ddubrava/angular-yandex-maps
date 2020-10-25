a63f06fc19cc64524744b8b4af125768c0d4dbe0
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2020-10-25 23:24:43 +0300

    refactor(app): fix eslint errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import {
   AngularYandexMapsModule,
   IConfig,
 } from '../../projects/angular8-yandex-maps/src/public-api';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { ClustererComponent } from './components/clusterer/clusterer.component';
 import { ControlsComponent } from './components/controls/controls.component';
 import { GeoobjectComponent } from './components/geoobject/geoobject.component';
 import { MapComponent } from './components/map/map.component';
 import { MultirouteComponent } from './components/multiroute/multiroute.component';
-import { NgModule } from '@angular/core';
 import { PanoramaComponent } from './components/panorama/panorama.component';
 import { PlacemarkComponent } from './components/placemark/placemark.component';
 
