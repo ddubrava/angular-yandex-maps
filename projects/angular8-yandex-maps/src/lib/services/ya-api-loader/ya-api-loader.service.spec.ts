d84ce1db7704c6cdbf0d084e12b90063d7646551
Daniil Dubrava <daniil.dubrava@yandex.ru>
2021-05-18 17:10:29 +0300

    feat(YaApiLoader): Make lang property optional, set ru_RU as default (#91)
    * feat(YaApiLoader): Make lang property optional, set ru_RU as default
    
    * test(YaApiLoader): Fix failed test
    
    * test(YaApiLoader): Add test case when lang isn't passed

--- projects/angular8-yandex-maps/src/lib/services/ya-api-loader/ya-api-loader.service.spec.ts
+++ projects/angular8-yandex-maps/src/lib/services/ya-api-loader/ya-api-loader.service.spec.ts
@@ -33,6 +33,23 @@ describe('YaApiLoaderService', () => {
     expect(mockDocument.body.appendChild).toHaveBeenCalled();
   });
 
+  it('should use default language if is not passed', () => {
+    const config: YaConfig = {
+      apikey: 'X-X-X',
+    };
+
+    service = new YaApiLoaderService(config, mockDocument);
+
+    const script = {} as HTMLScriptElement;
+    mockDocument.createElement.and.returnValue(script);
+
+    service.load();
+
+    expect(script.src).toContain('https://api-maps.yandex.ru/2.1');
+    expect(script.src).toContain('apikey=X-X-X');
+    expect(script.src).toContain('lang=ru_RU');
+  });
+
   it('should create script with provided options if config is passed', () => {
     const config: YaConfig = {
       apikey: 'X-X-X',
@@ -50,14 +67,16 @@ describe('YaApiLoaderService', () => {
 
     service.load();
 
-    expect(script.src).toBe(
-      'https://api-maps.yandex.ru/2.1/?apikey=X-X-X&lang=en_US&coordorder=latlong&load=package.full&mode=release',
-    );
+    expect(script.src).toContain('https://api-maps.yandex.ru/2.1');
+    expect(script.src).toContain('apikey=X-X-X');
+    expect(script.src).toContain('lang=en_US');
+    expect(script.src).toContain('coordorder=latlong');
+    expect(script.src).toContain('load=package.full');
+    expect(script.src).toContain('mode=release');
   });
 
   it('should create enterprise script if enterprise flag is passed', () => {
     const config: YaConfig = {
-      lang: 'ru_RU',
       enterprise: true,
     };
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 const mapConfig: YaConfig = {
-  lang: 'en_US',
   apikey: environment.apikey,
 };
 
