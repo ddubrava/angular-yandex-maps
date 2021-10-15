2b05a6363d0fe966188e9230c7d515551fff7c50
Daniil Dubrava <daniil.dubrava@yandex.ru>
2021-10-15 18:35:29 +0300

    Update eslint configuration (#126)
    * chore: change eslint config
    
    * chore: remove some tests from app.component.spec.ts
    
    * chore(deps): bump versions of dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
-/**
- * -----------------------------------------------------
- * NOTES ON CONFIGURATION STRUCTURE
- * -----------------------------------------------------
- *
- * Out of the box, ESLint does not support TypeScript or HTML. Naturally those are the two
- * main file types we care about in Angular projects, so we have to do a little extra work
- * to configure ESLint exactly how we need to.
- *
- * Fortunately, ESLint gives us an 'overrides' configuration option which allows us to set
- * different lint tooling (parser, plugins, rules etc) for different file types, which is
- * critical because our .ts files require a different parser and different rules to our
- * .html (and our inline Component) templates.
- */
 module.exports = {
   root: true,
   overrides: [
@@ -23,8 +9,10 @@ module.exports = {
       },
       extends: [
         'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates',
+        'airbnb-base',
         'airbnb-typescript/base',
-        'plugin:prettier/recommended',
+        'prettier',
       ],
       rules: {
         'import/prefer-default-export': 'off',
@@ -46,21 +34,10 @@ module.exports = {
         ],
       },
     },
-    // NOTE: WE ARE NOT APPLYING PRETTIER IN THIS OVERRIDE, ONLY @ANGULAR-ESLINT/TEMPLATE
     {
       files: ['*.html'],
       extends: ['plugin:@angular-eslint/template/recommended'],
       rules: {},
-    },
-    // NOTE: WE ARE NOT APPLYING @ANGULAR-ESLINT/TEMPLATE IN THIS OVERRIDE, ONLY PRETTIER
-    {
-      files: ['*.html'],
-      excludedFiles: ['*inline-template-*.component.html'],
-      extends: ['plugin:prettier/recommended'],
-      rules: {
-        // NOTE: WE ARE OVERRIDING THE DEFAULT CONFIG TO ALWAYS SET THE PARSER TO ANGULAR (SEE BELOW)
-        'prettier/prettier': ['error', { parser: 'angular' }],
-      },
     },
   ],
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -12,20 +12,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular-yandex-maps'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-yandex-maps');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'angular-yandex-maps app is running!',
-    );
   });
 });
