d30235353c9257431e2e5e63fcd43e1eddd045fc
ddubrava <daniil.dubrava@yandex.ru>
2021-11-25 22:11:58 +0000

    Deploying to gh-pages from  @ dbc5e26301564278846ee35fc97a6e6791d0b78d 🚀

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -80,13 +80,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="modules/AngularYandexMapsModule.html" data-type="entity-link" >AngularYandexMapsModule</a>
                                     <li class="chapter inner">
                                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                            'data-target="#components-links-module-AngularYandexMapsModule-0ab137c1761ca7f4a19c4b7aabaa9190"' : 'data-target="#xs-components-links-module-AngularYandexMapsModule-0ab137c1761ca7f4a19c4b7aabaa9190"' }>
+                                            'data-target="#components-links-module-AngularYandexMapsModule-6f5b103ca9c176f4542ea0074802bd68b670cb39d83a451aae15eefa63c15a7dfc7cb51f3dee7c843dfef1a5f543d8751c0cdd0e8698e68dc213ed6623c2cfd7"' : 'data-target="#xs-components-links-module-AngularYandexMapsModule-6f5b103ca9c176f4542ea0074802bd68b670cb39d83a451aae15eefa63c15a7dfc7cb51f3dee7c843dfef1a5f543d8751c0cdd0e8698e68dc213ed6623c2cfd7"' }>
                                             <span class="icon ion-md-cog"></span>
                                             <span>Components</span>
                                             <span class="icon ion-ios-arrow-down"></span>
                                         </div>
-                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-AngularYandexMapsModule-0ab137c1761ca7f4a19c4b7aabaa9190"' :
-                                            'id="xs-components-links-module-AngularYandexMapsModule-0ab137c1761ca7f4a19c4b7aabaa9190"' }>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-AngularYandexMapsModule-6f5b103ca9c176f4542ea0074802bd68b670cb39d83a451aae15eefa63c15a7dfc7cb51f3dee7c843dfef1a5f543d8751c0cdd0e8698e68dc213ed6623c2cfd7"' :
+                                            'id="xs-components-links-module-AngularYandexMapsModule-6f5b103ca9c176f4542ea0074802bd68b670cb39d83a451aae15eefa63c15a7dfc7cb51f3dee7c843dfef1a5f543d8751c0cdd0e8698e68dc213ed6623c2cfd7"' }>
                                             <li class="link">
                                                 <a href="components/YaClustererComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >YaClustererComponent</a>
                                             </li>
@@ -97,13 +97,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                     </li>
                                 <li class="chapter inner">
                                     <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                        'data-target="#directives-links-module-AngularYandexMapsModule-0ab137c1761ca7f4a19c4b7aabaa9190"' : 'data-target="#xs-directives-links-module-AngularYandexMapsModule-0ab137c1761ca7f4a19c4b7aabaa9190"' }>
+                                        'data-target="#directives-links-module-AngularYandexMapsModule-6f5b103ca9c176f4542ea0074802bd68b670cb39d83a451aae15eefa63c15a7dfc7cb51f3dee7c843dfef1a5f543d8751c0cdd0e8698e68dc213ed6623c2cfd7"' : 'data-target="#xs-directives-links-module-AngularYandexMapsModule-6f5b103ca9c176f4542ea0074802bd68b670cb39d83a451aae15eefa63c15a7dfc7cb51f3dee7c843dfef1a5f543d8751c0cdd0e8698e68dc213ed6623c2cfd7"' }>
                                         <span class="icon ion-md-code-working"></span>
                                         <span>Directives</span>
                                         <span class="icon ion-ios-arrow-down"></span>
                                     </div>
-                                    <ul class="links collapse" ${ isNormalMode ? 'id="directives-links-module-AngularYandexMapsModule-0ab137c1761ca7f4a19c4b7aabaa9190"' :
-                                        'id="xs-directives-links-module-AngularYandexMapsModule-0ab137c1761ca7f4a19c4b7aabaa9190"' }>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="directives-links-module-AngularYandexMapsModule-6f5b103ca9c176f4542ea0074802bd68b670cb39d83a451aae15eefa63c15a7dfc7cb51f3dee7c843dfef1a5f543d8751c0cdd0e8698e68dc213ed6623c2cfd7"' :
+                                        'id="xs-directives-links-module-AngularYandexMapsModule-6f5b103ca9c176f4542ea0074802bd68b670cb39d83a451aae15eefa63c15a7dfc7cb51f3dee7c843dfef1a5f543d8751c0cdd0e8698e68dc213ed6623c2cfd7"' }>
                                         <li class="link">
                                             <a href="directives/YaControlDirective.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >YaControlDirective</a>
                                         </li>
