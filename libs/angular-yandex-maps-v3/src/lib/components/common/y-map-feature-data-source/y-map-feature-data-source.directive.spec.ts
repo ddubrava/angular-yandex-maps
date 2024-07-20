9056ea511f478bd2aa4ef082b4dd4e9c987941ac
Daniil Dubrava <dubrava.daniil@gmail.com>
2024-07-20 20:18:21 +0200

    feat: support YMapFeatureDataSource component (#250)

---
+++ libs/angular-yandex-maps-v3/src/lib/components/common/y-map-feature-data-source/y-map-feature-data-source.directive.spec.ts
@@ -0,0 +1,109 @@
+import { Component, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { YMapFeatureDataSourceProps } from '@yandex/ymaps3-types';
+import { BehaviorSubject } from 'rxjs';
+
+import {
+  mockYMapFeatureDataSourceConstructor,
+  mockYMapFeatureDataSourceInstance,
+  mockYMapInstance,
+} from '../../../../test-utils';
+import { YReadyEvent } from '../../../types/y-ready-event';
+import { YMapComponent } from '../y-map/y-map.component';
+import { YMapFeatureDataSourceDirective } from './y-map-feature-data-source.directive';
+
+@Component({
+  standalone: true,
+  imports: [YMapFeatureDataSourceDirective],
+  template: '<y-map-feature-data-source [props]="props" />',
+})
+class MockHostComponent {
+  @ViewChild(YMapFeatureDataSourceDirective, { static: true })
+  feature!: YMapFeatureDataSourceDirective;
+
+  props: YMapFeatureDataSourceProps = {
+    id: 'a-source',
+  };
+}
+
+describe('YMapFeatureDataSourceDirective', () => {
+  let component: YMapFeatureDataSourceDirective;
+  let mockComponent: MockHostComponent;
+  let fixture: ComponentFixture<MockHostComponent>;
+
+  let mapInstance: ReturnType<typeof mockYMapInstance>;
+  let sourceInstance: ReturnType<typeof mockYMapFeatureDataSourceInstance>;
+  let sourceConstructorMock: jest.Mock;
+
+  beforeEach(async () => {
+    mapInstance = mockYMapInstance();
+
+    await TestBed.configureTestingModule({
+      imports: [MockHostComponent],
+      providers: [
+        {
+          provide: YMapComponent,
+          useValue: {
+            map$: new BehaviorSubject(mapInstance),
+          },
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MockHostComponent);
+    mockComponent = fixture.componentInstance;
+    component = mockComponent.feature;
+
+    sourceInstance = mockYMapFeatureDataSourceInstance();
+    sourceConstructorMock = mockYMapFeatureDataSourceConstructor(sourceInstance);
+  });
+
+  afterEach(() => {
+    (window as any).ymaps3 = undefined;
+  });
+
+  it('should create entity', () => {
+    fixture.detectChanges();
+
+    expect(sourceConstructorMock).toHaveBeenCalledWith(mockComponent.props);
+    expect(mapInstance.addChild).toHaveBeenCalledWith(sourceInstance);
+  });
+
+  it('should emit ready on load', () => {
+    jest.spyOn(component.ready, 'emit');
+    fixture.detectChanges();
+
+    const readyEvent: YReadyEvent = {
+      ymaps3: (window as any).ymaps3,
+      entity: sourceInstance,
+    };
+
+    expect(component.ready.emit).toHaveBeenCalledWith(readyEvent);
+  });
+
+  it('should pass inputs to constructor', () => {
+    const props: YMapFeatureDataSourceProps = {
+      id: 'b-source',
+    };
+
+    mockComponent.props = props;
+
+    fixture.detectChanges();
+
+    expect(sourceConstructorMock).toHaveBeenCalledWith(props);
+  });
+
+  it('should update props input after init', () => {
+    fixture.detectChanges();
+
+    const props: YMapFeatureDataSourceProps = {
+      id: 'c-source',
+    };
+
+    mockComponent.props = props;
+
+    fixture.detectChanges();
+
+    expect(sourceInstance.update).toHaveBeenCalledWith(props);
+  });
+});
---
+++ libs/angular-yandex-maps-v3/src/lib/components/common/y-map-feature-data-source/y-map-feature-data-source.directive.ts
@@ -0,0 +1,77 @@
+import {
+  Directive,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { YMapFeatureDataSource, YMapFeatureDataSourceProps } from '@yandex/ymaps3-types';
+import { Subject, takeUntil } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
+import { YReadyEvent } from '../../../types/y-ready-event';
+import { YMapComponent } from '../y-map/y-map.component';
+
+/**
+ * This component wraps the [ymaps3.YMapFeatureDataSource](https://yandex.ru/dev/jsapi30/doc/ru/ref/#class-ymapfeaturedatasource) class from the Yandex.Maps API.
+ * All component inputs are named the same as the API class constructor arguments.
+ *
+ * ```html
+ * <y-map
+ *   [props]="{
+ *     location: {
+ *       center: [-0.127696, 51.507351],
+ *       zoom: 9,
+ *     },
+ *   }"
+ * >
+ *   <y-map-feature-data-source [props]="{ id: 'clusterer-source' }" />
+ * </y-map>
+ * ```
+ */
+@Directive({
+  selector: 'y-map-feature-data-source',
+  standalone: true,
+})
+export class YMapFeatureDataSourceDirective implements OnInit, OnDestroy, OnChanges {
+  private readonly destroy$ = new Subject<void>();
+
+  private source?: YMapFeatureDataSource;
+
+  /**
+   * See the API entity documentation for detailed information. Supports ngOnChanges.
+   * {@link https://yandex.ru/dev/jsapi30/doc/ru/ref/#YMapFeatureDataSourceProps}
+   */
+  @Input({ required: true }) props!: YMapFeatureDataSourceProps;
+
+  /**
+   * The entity instance is created. This event runs outside an Angular zone.
+   */
+  @Output() ready: EventEmitter<YReadyEvent<YMapFeatureDataSource>> = new EventEmitter<
+    YReadyEvent<YMapFeatureDataSource>
+  >();
+
+  constructor(private readonly yMapComponent: YMapComponent) {}
+
+  ngOnInit() {
+    this.yMapComponent.map$.pipe(filter(Boolean), takeUntil(this.destroy$)).subscribe((map) => {
+      this.source = new ymaps3.YMapFeatureDataSource(this.props);
+      map.addChild(this.source);
+      this.ready.emit({ ymaps3, entity: this.source });
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.source) {
+      this.source.update(changes['props'].currentValue);
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
