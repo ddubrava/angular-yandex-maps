9c772d15a942cdc8fbbb4c85b1c509da60f6ca45
Daniil Dubrava <dubrava.daniil@gmail.com>
2024-07-20 19:00:54 +0200

    feat: support YMapLayer component

---
+++ libs/angular-yandex-maps-v3/src/lib/components/layers/y-map-layer/y-map-layer.directive.ts
@@ -0,0 +1,84 @@
+import {
+  Directive,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { YMapLayer, YMapLayerProps } from '@yandex/ymaps3-types';
+import { Subject, takeUntil } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
+import { YReadyEvent } from '../../../types/y-ready-event';
+import { YMapComponent } from '../../common/y-map/y-map.component';
+
+/**
+ * This component wraps the [ymaps3.YMapLayer](https://yandex.ru/dev/jsapi30/doc/ru/ref/#class-ymaplayer) class from the Yandex.Maps API.
+ * All component inputs are named the same as the API class constructor arguments.
+ *
+ * ```html
+ * <y-map
+ *   [props]="{
+ *     location: {
+ *       center: [-0.127696, 51.507351],
+ *       zoom: 10,
+ *     },
+ *   }"
+ * >
+ *   <y-map-default-scheme-layer />
+ *
+ *   <y-map-layer
+ *     [props]="{
+ *       type: 'markers',
+ *       zIndex: 1800,
+ *     }"
+ *   />
+ * </y-map>
+ * ```
+ */
+@Directive({
+  selector: 'y-map-layer',
+  standalone: true,
+})
+export class YMapLayerDirective implements OnInit, OnDestroy, OnChanges {
+  private readonly destroy$ = new Subject<void>();
+
+  private layer?: YMapLayer;
+
+  /**
+   * See the API entity documentation for detailed information. Supports ngOnChanges.
+   * {@link https://yandex.ru/dev/jsapi30/doc/ru/ref/#YMapLayerProps}
+   */
+  @Input({ required: true }) props!: YMapLayerProps;
+
+  /**
+   * The entity instance is created. This event runs outside an Angular zone.
+   */
+  @Output() ready: EventEmitter<YReadyEvent<YMapLayer>> = new EventEmitter<
+    YReadyEvent<YMapLayer>
+  >();
+
+  constructor(private readonly yMapComponent: YMapComponent) {}
+
+  ngOnInit() {
+    this.yMapComponent.map$.pipe(filter(Boolean), takeUntil(this.destroy$)).subscribe((map) => {
+      this.layer = new ymaps3.YMapLayer(this.props);
+      map.addChild(this.layer);
+      this.ready.emit({ ymaps3, entity: this.layer });
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.layer) {
+      this.layer.update(changes['props'].currentValue);
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
