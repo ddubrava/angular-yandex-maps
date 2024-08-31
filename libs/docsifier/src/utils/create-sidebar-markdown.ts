f477e944efa4fd54b8a3e948f527a3bac8298028
Daniil Dubrava <dubrava.daniil@gmail.com>
2024-09-01 00:54:50 +0200

    docs: add categories to sidebar

--- libs/docsifier/src/utils/create-sidebar-markdown.ts
+++ libs/docsifier/src/utils/create-sidebar-markdown.ts
@@ -5,11 +5,45 @@ import dedent from 'ts-dedent';
 import { CompodocEntity } from '../types/compodoc-entity';
 import { CompodocDocumentation } from '../types/compodocDocumentation';
 
+const sourceCodeDirectory = 'lib';
+
 /**
  * Creates a string with Markdown links joined by \n.
  */
-const createLinks = (entities: CompodocEntity[], path: string): string =>
-  entities.map(({ name }) => `- [${name}](${path}/${name})`).join('\n');
+const createLinks = (entities: CompodocEntity[], path: string): string => {
+  const filesByCategories: Record<string, string[]> = {};
+
+  for (const { name, file } of entities) {
+    const parts = file.split('/');
+
+    const rootIndex = parts.indexOf(sourceCodeDirectory);
+
+    // It's hardcoded as hell, but it's the only way to get this category.
+    // +2 to skip lib/components.
+    // -2 to skip the component directory.
+    const category = parts.slice(rootIndex + 2, parts.length - 2)[0] ?? '';
+
+    if (!filesByCategories[category]) {
+      filesByCategories[category] = [];
+    }
+
+    filesByCategories[category].push(`- [${name}](${path}/${name})`);
+  }
+
+  const categories = Object.entries(filesByCategories).map(([category, items]) => {
+    const itemLinks = items.map((item) => `  ${item}`).join('\n');
+
+    if (category) {
+      const categoryHeader = `- **${category}**`;
+
+      return `${categoryHeader}\n${itemLinks}`;
+    }
+
+    return itemLinks;
+  });
+
+  return categories.join('\n');
+};
 
 /**
  * Creates a _sidebar.md file from passed data.
